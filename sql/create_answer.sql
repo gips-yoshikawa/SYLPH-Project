2397a063fbc6ca79b04cdb4bd8af937faa912a21
gips-yoshikawa <102131883+gips-yoshikawa@users.noreply.github.com>
2022-05-18 18:39:43 +0900

    update sql

--- sql/create_answer.sql
+++ sql/create_answer.sql
@@ -1,8 +1,8 @@
 create table answer(
-question_number integer,
 subject_number integer,
+question_number integer,
 question_answer integer,
 primary key  (subject_number,question_number),
-constraint qs_number_fk foreign key (question_number,subject_number) 
-references question(question_number,subject_number)
+constraint qs_number_fk foreign key (subject_number, question_number) 
+references question(subject_number, question_number)
 );
